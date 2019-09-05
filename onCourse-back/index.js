4d4c2257075e32489b33b9a460643dbec74041a5
Lewis Birks <lewisb@kainos.com>
2019-09-05 11:50:35 +0100

    Added error handling function

--- onCourse-back/index.js
+++ onCourse-back/index.js
@@ -7,8 +7,18 @@ app.use(bodyParser.urlencoded());
 
 const db = require('./db.js');
 
+function handleError(err, req, res) {
+  if (err.errno === 3819) err.code = "ER_CHECK_CONSTRAINT_VIOLATED";
+  console.error(`${err.errno} (${err.code}) : ${err.sqlMessage}`);
+  res.status(500).send({
+    message: 'Database error. ' + err.sqlMessage
+  });
+}
+
 app.listen(8002, function () {
   console.log('express started on port 8002');
 });
 
+app.post('/course', function (req, res) {
 
+});
