dd963489bfbf1f1b8343cc008df54991333425ae
Rewaz <klv96@outlook.com>
2019-09-05 13:34:11 +0100

    done the routing

--- onCourse/src/app/app-routing.module.ts
+++ onCourse/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddCourseComponent } from './add-course/add-course.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: AddCourseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AddCourseComponent]
--- onCourse/src/app/app.module.ts
+++ onCourse/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    AddCourseComponent,
-    HeaderComponent
+    // AddCourseComponent,
+    HeaderComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
