d8504b219d9db98cbc9db5d639438002ee4b0c67
Clare Buckley <clarebu@kainos.com>
2019-09-05 12:02:34 +0100

    add sql file to create database

---
+++ onCourse-db/onCourse-database-setup.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS onCourse_db;
+CREATE DATABASE IF NOT EXISTS onCourse_db;
+USE onCourse_db;
+
+CREATE TABLE IF NOT EXISTS Course(
+	course_id smallint PRIMARY KEY auto_increment,
+    course_title VARCHAR(100) NOT NULL,
+    location ENUM("Belfast", "Derry", "Dublin", "London", "Gdansk", "Amsterdam", "Boston", "Frankfurt") NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    start_date DATE NOT NULL,
+    duration_hours DECIMAL(4,2) NOT NULL,
+    trainer_names VARCHAR(100) NOT NULL,
+    target_audience VARCHAR(200) NOT NULL,
+    CONSTRAINT `start_date` CHECK (`start_date` > now())
+);
