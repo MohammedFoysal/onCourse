5a9eea8dbd231c1584f41ee7a664767eff44b8e9
Clare Buckley <clarebu@kainos.com>
2019-09-05 12:51:19 +0100

    remove trainer and trainer_course tables, revert to trainers stored as strings in course table

--- onCourse-db/onCourse-database-setup.sql
+++ onCourse-db/onCourse-database-setup.sql
@@ -10,19 +10,20 @@ CREATE TABLE IF NOT EXISTS Course(
     description VARCHAR(300) NOT NULL,
     start_date DATE NOT NULL,
     duration_hours DECIMAL(4,2) NOT NULL,
-    target_audience VARCHAR(200) NOT NULL
+    target_audience VARCHAR(200) NOT NULL,
+    trainer_names VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Trainer(
-	trainer_id smallint PRIMARY KEY auto_increment,
-	f_name VARCHAR(80),
-    l_name VARCHAR(100)
-);
+-- CREATE TABLE IF NOT EXISTS Trainer(
+-- 	trainer_id smallint PRIMARY KEY auto_increment,
+-- 	f_name VARCHAR(80),
+--     l_name VARCHAR(100)
+-- );
 
-CREATE TABLE Course_Trainer(
-	trainer_id smallint,
-    course_id smallint,
-    PRIMARY KEY(trainer_id, course_id),
-    FOREIGN KEY(trainer_id) REFERENCES Trainer(trainer_id),
-    FOREIGN KEY(course_id) REFERENCES Course(course_id)
-);
+-- CREATE TABLE Course_Trainer(
+-- 	trainer_id smallint,
+--     course_id smallint,
+--     PRIMARY KEY(trainer_id, course_id),
+--     FOREIGN KEY(trainer_id) REFERENCES Trainer(trainer_id),
+--     FOREIGN KEY(course_id) REFERENCES Course(course_id)
+-- );
